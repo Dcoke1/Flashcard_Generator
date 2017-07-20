45524616d0a2125b1035ef092c87c8a99f41dd5a
Douglas Coke <CEO@Douglass-MacBook-Pro.local>
2017-07-20 13:38:02 -0400

    update1.5

--- BasicCard.js
+++ BasicCard.js
@@ -10,9 +10,28 @@ var A = [];
 var count = 0;
 var correct = 0;
 
+// Goes through loop and gets category names for prompt
 for (var i = 0; i < facts.length; i++) {
 	categories.push(facts[i].name);
 
 	 console.log(facts[i]);
 }
+
+console.log("*********************************************************");
+
+//inquirer function for choosing a category
+
+function Game () {
+	inquirer.prompt([
+	{
+		type: "list",
+		message: "** Choose a Topic for your quiz =] **",
+		choices: categories,
+		name: "Game"
+	},
+		]).then(function(response) {
+
+		});
+}
+
 
