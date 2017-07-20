a81cfd18e88810905997ca352d70f73b3159c329
Douglas Coke <CEO@Douglass-MacBook-Pro.local>
2017-07-20 14:02:00 -0400

    Final

--- BasicCard.js
+++ BasicCard.js
@@ -6,7 +6,7 @@ var quiz = require("./questions.js");
 
 var categories = [];
 var facts = questions.categor
-var A = [];
+var Q = [];
 var count = 0;
 var correct = 0;
 
@@ -20,7 +20,6 @@ for (var i = 0; i < facts.length; i++) {
 console.log("*********************************************************");
 
 //inquirer function for choosing a category
-
 function Game () {
 	inquirer.prompt([
 	{
@@ -30,8 +29,72 @@ function Game () {
 		name: "Game"
 	},
 		]).then(function(response) {
+			var loc;
 
+			for (var i = 0; i < facts.length; i++) if (facts[i].name === response.categor) loc = i;
+			for (var i = 0; i < 5; i++) Q[i] = new flash_card(facts[loc][i].A, facts[loc][i].A);
+
+			trivia();
 		});
 }
+
+//Restart Function
+function restart () {
+	inquirer.prompt([
+	{
+		type: "confirm",
+		message: "** Play Again? =] **",
+		default: true,
+		name: "restart"
+
+	},
+
+		]).then(function(response) {
+			if(response.restart){
+				count = 0; correct = 0; Game();
+			}
+		});
+}
+
+//Run flash_card trivia
+function trivia () {
+
+	if (count < 5) {
+		
+		Q[count].clozeDone();
+		
+		console.log("\nQuestion " + (count + 1))
+		console.log("***************");
+
+//Prompt Question
+		inquirer.prompt([
+		    {
+		      	type: "input",
+		      	message: Q[count].partial,	
+		      	name: "answer"
+		    },
+
+				]).then(function(response) {
+	
+		if (response.answer.trim().toLowerCase() == Q[count].cloze){
+			console.log("\nCorrect!\n" + Q[count].fullText);
+			correct++
+		} 
+		else console.log("\nIncorrect\n" + Q[count].fullText);
+		count++ 
+		
+		trivia();			
+		});
+	}
+	else {
+		console.log("\nResults\n----------------\nYou Answered " + correct + " correct.\n");
+		//Ask to restart game 
+		restart();
+	}
+}
+
+//Run App
+Game();
+
 
 
